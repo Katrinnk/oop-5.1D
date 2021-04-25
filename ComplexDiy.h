5706456d367e6d4fc45b60535a3739d4f17c944a
Katrinnk <katerin69llobster@gmail.com>
2021-04-25 22:37:42 +0300

    5.1D

---
+++ ComplexDiy.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Complex.h"
+class ComplexDiy :public Complex
+{
+public:
+	ComplexDiy();
+	ComplexDiy(double, double, double, double);
+	ComplexDiy(const ComplexDiy&);
+
+	void Sub();
+	void Div();
+	void Conj1();
+	void Conj2();
+};
---
+++ ComplexPoriv.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Complex.h"
+#pragma once
+class ComplexPoriv :public Complex
+{
+public:
+	ComplexPoriv();
+
+	friend bool operator > (ComplexPoriv&, ComplexPoriv&);
+	friend bool operator < (ComplexPoriv&, ComplexPoriv&);
+	friend bool operator == (ComplexPoriv&, ComplexPoriv&);
+
+};
+
